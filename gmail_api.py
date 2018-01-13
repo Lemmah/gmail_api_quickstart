4ac93c2c431eb70760fe8a6a78df6acbea62acef
Lemmah <lemayiannakolah@gmail.com>
2018-01-13 23:30:41 +0300

    ch(refactor): Create a Gmail Account blueprint.

---
+++ gmail_api.py
@@ -0,0 +1,36 @@
+class GmailAccount:
+  """Peform all gmail operations from the console using gmail API
+  1. Get all gmail messages: get by ID, get by Tags
+  2. Modify all gmail messages: modify labels
+  3. Compose and send a new message
+  """
+  
+  def __init__(self):
+    """Construct all the instance variables
+    """
+    pass
+
+  def get_msg(self, *args, **kwargs):
+    """All the various ways of getting a message
+    """
+    pass
+
+  def modify_msg(self, *args, **kwargs):
+    """All the various ways of modifying a message
+    """
+    pass
+  
+  def send_msg(self, *args, **kwargs):
+    """All the ways of sending a message
+    """
+    pass
+  
+  def get_all_labels(self, *args, **kwargs):
+    """Get all message labels
+    """
+    pass
+  
+  def create_label(self, *args, **kwargs):
+    """Create a new message label
+    """
+    pass
